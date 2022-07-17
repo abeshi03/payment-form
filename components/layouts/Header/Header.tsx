1db55c21d79c5ccdf563684fa6d0d89be6521502
abeshi03 <k_913411@yahoo.co.jp>
2022-07-17 22:58:49 +0900

    fix

--- components/layouts/Header/Header.tsx
+++ components/layouts/Header/Header.tsx
@@ -2,19 +2,24 @@ import { FC, memo } from "react";
 import styles from "./Header.module.scss";
 import { useRecoilValue } from "recoil";
 import { cartState } from "../../../stores/cart";
+import Link from "next/link";
 
 export const Header: FC = memo(() => {
   const cart = useRecoilValue(cartState);
-  console.log(cart);
+
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.cart}>
-          <span>カート</span>
-          {cart.products.length >= 1 && <span className={styles.count}>{cart.products.length}</span>}
+    <>
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <Link className={styles.cart} href={"/payments"}>
+            <a>
+              <span>カート</span>
+              {cart.products.length >= 1 && <span className={styles.count}>{cart.products.length}</span>}
+            </a>
+          </Link>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 });
 
--- pages/payments/index.tsx
+++ pages/payments/index.tsx
@@ -11,15 +11,41 @@ import { PaymentControlGroup } from "../../components/organisms/ControlGroups/Pa
 
 /* --- types -------------------------------------------------------------------------------------------------------- */
 import { PaymentDataInputValues } from "./pageSettings";
+import { useRecoilValue } from "recoil";
+import { cartState } from "../../stores/cart";
 
 const PaymentsPage: NextPage = () => {
+  const cart = useRecoilValue(cartState);
+
   const submit: SubmitHandler<PaymentDataInputValues> = useCallback(async (inputValues) => {
     console.log(inputValues);
   }, []);
 
+  const cartItems = () => {
+    if (cart.products.length === 0) return <p>カートにアイテムが入っていません</p>;
+    return (
+      <div className={styles.cartItemsFlow}>
+        {cart.products.map((product) => (
+          <div key={product.id} className={styles.cartItem}>
+            <div
+              className={styles.image}
+              role="img"
+              style={{ backgroundImage: `url(https://dummyimage.com/100x100/03488d/fff})` }}
+            ></div>
+            <h2 className={styles.name}>{product.name}</h2>
+            <p className={styles.price}>{product.price}円</p>
+          </div>
+        ))}
+        <p>合計金額{cart.totalPrice}円</p>
+      </div>
+    );
+  };
+
   return (
     <div className={styles.paymentsPage}>
-      <h1 className={styles.heading}>決済ページ</h1>
+      <h2 className={styles.heading}>カート</h2>
+      {cartItems()}
+      <h2 className={styles.heading}>購入する</h2>
       <PaymentControlGroup className={styles.paymentControlGroup} submitFunction={submit} />
     </div>
   );
